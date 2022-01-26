5b37ea389c843632b2b28c8889094cfef4fd004c
sunnyguohua <sunnyguohua@gmail.com>
2022-01-26 16:11:03 +0800

    updated for TCB live demo

--- undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
+++ undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
@@ -54,7 +54,7 @@ public class ServletServer {
                     .setDeploymentName("test.war")
                     .addServlets(
                             servlet("MessageServlet", MessageServlet.class)
-                                    .addInitParam("message", "Hello World")
+                                    .addInitParam("message", "Hello World from TCB live demo @2022012616!")
                                     .addMapping("/*"),
                             servlet("MyServlet", MessageServlet.class)
                                     .addInitParam("message", "MyServlet")
